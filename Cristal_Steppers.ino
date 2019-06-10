e706aaea35d3a4bdf79b02bb3523e48b59d469ab
Ignacio C. Stolkiner <35274305+IgnacioCastroStolkiner@users.noreply.github.com>
2019-06-09 22:19:34 -0300

    Comenté el Código
    Comenté el código de forma más extensiva para que se entienda mejor.

--- Cristal_Steppers.ino
+++ Cristal_Steppers.ino
@@ -2,27 +2,29 @@
 #include <Wire.h>
 #include <AccelStepper.h>
 
-#define MAX_LENGTH 12
-int SL_ADDR;
+#define MAX_LENGTH 12 //Máximo largo del comando
+int SL_ADDR;//Dirección del Arduino
 
-byte command[MAX_LENGTH];
-byte command_length;
-boolean processingMessage = false;
-boolean commandComplete = false;
+byte command[MAX_LENGTH]; //Array comando
+byte command_length;      //Largo del comando 
+boolean processingMessage = false; //Estado recibiendo comando
+boolean commandComplete = false;  //Estado comando completo
 
-const long MOTOR_STEPS = 200;
-const long MICROSTEPS = 16;
+const long MOTOR_STEPS = 200;//
+const long MICROSTEPS = 16; //Microsteps
 
-const int stepPins[5] = {54, 60, 46, 26, 36};
-const int dirPins[5] = {55, 61, 48, 28, 34};
-const int enPins[5] = {38, 56, 62, 24, 30};
+const int stepPins[5] = {54, 60, 46, 26, 36}; //Pines de target
+const int dirPins[5] = {55, 61, 48, 28, 34};  //Pines de dirección
+const int enPins[5] = {38, 56, 62, 24, 30};   //Pines de enable
 
+//Inicialización objetos Stepper
 AccelStepper  stepper1(AccelStepper::DRIVER,  stepPins[0],  dirPins[0]);
 AccelStepper  stepper2(AccelStepper::DRIVER,  stepPins[1],  dirPins[1]);
 AccelStepper  stepper3(AccelStepper::DRIVER,  stepPins[2],  dirPins[2]);
 AccelStepper  stepper4(AccelStepper::DRIVER,  stepPins[3],  dirPins[3]);
 AccelStepper  stepper5(AccelStepper::DRIVER,  stepPins[4],  dirPins[4]);
 
+//Array Steppers
 AccelStepper* steppers[] = {
   &stepper1,
   &stepper2,
@@ -31,16 +33,16 @@ AccelStepper* steppers[] = {
   &stepper5,
 };
 
-long MAX_SPEED = 500 * MICROSTEPS;
-long ACCEL = 300 * MICROSTEPS;
-long NSTEPS = 500 * MICROSTEPS;
-int NMOT = 5;
+long MAX_SPEED = 500 * MICROSTEPS; //Velicidad maxima
+long ACCEL = 300 * MICROSTEPS;    //Aceleración
+long NSTEPS = 500 * MICROSTEPS;   
+int NMOT = 5; //Número de motores
 
 void setup()
 {
-  SL_ADDR = EEPROM.read(0);
+  SL_ADDR = EEPROM.read(0); //Dirección en ROM
   TWAR = (SL_ADDR << 1) | 1;  // enable broadcasts to be received
-
+  //I2C
   Wire.begin(SL_ADDR);
   Wire.onReceive(receiveEvent); // Registra evento de recibir comando
   Wire.onRequest(requestEvent); // Registra evento de request de posicion
@@ -54,27 +56,30 @@ void setup()
 }
 
 void loop() {
+  //Procesa comando cuando terminó de recibir
   if (commandComplete) {
     commandComplete = false;
     processCommand();
   }
   for (int i = 0; i < 5; i++) {
-    if (steppers[i]->distanceToGo() == 0) steppers[i]->disableOutputs();
+    if (steppers[i]->distanceToGo() == 0) steppers[i]->disableOutputs();//Solo run cuando aún no llegó a target
     else steppers[i]->run();
   }
 }
 
+//Recibir comando
 void receiveEvent(int numBytes)
 {
+  //Chequear que no se trabe con comando anterior
   if (processingMessage) {
     unsigned long t = millis();
     while (processingMessage) {
       if (millis() > t + 3000) break;
     }
   }
-  processingMessage = true;
-  byte tmpcommand[MAX_LENGTH];  
-  for (byte n = 0; n < numBytes; n++) {
+  processingMessage = true; //Procesando  comando
+  byte tmpcommand[MAX_LENGTH];  //Buffer comando
+  for (byte n = 0; n < numBytes; n++) { //Cargar en buffer
     if (n < MAX_LENGTH)
     {
       tmpcommand[n] = Wire.read();
@@ -105,15 +110,15 @@ void requestEvent() {
   interrupts();
 }
 
-//Procesa el comando
+//Procesa el comando, parse
 void processCommand() {
   //registro (opcional)
   byte p_command[MAX_LENGTH];
   memcpy(p_command, command, MAX_LENGTH); 
-  //byte REG = p_command[0];
-  char CMD = p_command[1];
-  unsigned char nmot = p_command[2];
-  int value = (p_command[3] << 8) | (p_command[4]);
+  //byte REG = p_command[0]; //Byte 0 registro
+  char CMD = p_command[1]; //Byte 1 comando
+  unsigned char nmot = p_command[2];//Byte 2 motor
+  int value = (p_command[3] << 8) | (p_command[4]); //Valor en comandos 2 y 3
   switch (CMD) {
     case 's': //Move to target un motor
       steppers[nmot]->enableOutputs();
